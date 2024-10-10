7117abb1de1fd9620feda040ce126b5ac1a04daa
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-10-10 13:12:47 +0200

    feat: first web server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("starting server on :3500")
+
+	err := http.ListenAndServe(":3500", mux)
+	log.Fatal(err)
 }
