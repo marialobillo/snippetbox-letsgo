f2a59e2f4036215e45430333a489dbbfde77e675
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-10-11 15:20:48 +0200

    feat: add dotenv for the project

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,27 +1,51 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"net/http"
+	"os"
+	"github.com/joho/godotenv"
 )
 
+type config struct {
+	addr      string
+	staticUIDir string
+	staticDir string
+}
+
 func main() {
-	addr := flag.String("addr", ":3500", "HTTP network address")
-	flag.Parse()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	cfg := config{
+		addr:       getEnv("ADDR", ":3500"),
+		staticUIDir: getEnv("STATIC_UI_DIR", "./ui/static"),
+		staticDir:  getEnv("STATIC_DIR", "/static"),
+	}
 
     mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.Dir(cfg.staticUIDir))
+	mux.Handle("GET /static/", http.StripPrefix(cfg.staticDir, fileServer))
 
     mux.HandleFunc("GET /{$}", home)
     mux.HandleFunc("GET /snippet/view/{id}", snippetView)
     mux.HandleFunc("GET /snippet/create", snippetCreate)
     mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-    log.Printf("starting server on %s", *addr)
+    log.Printf("starting server on %s", cfg.addr)
     
-    err := http.ListenAndServe(*addr, mux)
+    err = http.ListenAndServe(cfg.addr, mux)
     log.Fatal(err)
+}
+
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/marialobillo/snippetbox-letsgo
 
 go 1.23.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
